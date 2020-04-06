53eee914ebfbb556042efcba5a0d8fb9957a241c
lipeng <247934357@qq.com>
2020-04-06 13:03:43 +0800

    删除旧的分支代码

--- src/setupProxy.js
+++
@@ -1,5 +0,0 @@
-const proxy = require('http-proxy-middleware');
-
-module.exports = function (app) {
-    app.use(proxy('/AssetWeb', {target: 'http://172.16.131.31:8005'}))
-};
--- src/views/403.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class P403 extends React.Component {
-
-    render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
-        return undefined;
-    }
-
-}
