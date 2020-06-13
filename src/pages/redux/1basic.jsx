892a4896b784630547c0d551469c6867630821ef
lipeng <247934357@qq.com>
2020-06-13 13:44:09 +0800

    [feat] 增加router代码和简单redux代码

---
+++ src/pages/redux/1basic.jsx
@@ -0,0 +1,50 @@
+import { createStore } from 'redux'
+import React from 'react';
+
+// 处理函数
+function counter(state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+}
+
+// 创建一个store
+let store = createStore(counter)
+// 当数据发生变化的时候触发
+store.subscribe(()=> {console.log(store.getState())});
+
+class HelloWorld extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    plus = () => {
+      // 触发变更
+      store.dispatch({ type: 'INCREMENT' })
+    }
+
+    subtract = () => {
+      // 触发变更
+      store.dispatch({ type: 'DECREMENT' })
+    }
+
+    render() {
+        return <div>
+          <div className="name"><button onClick={this.plus}>++</button></div>
+          <div className="age"><button onClick={this.subtract}>--</button></div>
+          <div>
+            <div>number:{store.getState()}</div>
+          </div>
+        </div>;
+    }
+}
+
+export default HelloWorld;
+
--- src/pages/router/2primary.js
+++ src/pages/router/2primary.js
@@ -4,6 +4,29 @@
  *  route matchers, eg: <Route> and <Switch>
  *  and navigation, eg: <Link>, <NavLink>, and <Redirect>
  * 
- * TODO: 关于这块内容的详细解释： https://reacttraining.com/react-router/web/guides/primary-components
+ * Routers
+ *   <BrowserRouter> and <HashRouter>
+ *    主要区别：
+ *      browserRouter虽然看上去更好看，但是需要服务端配合
+ *      hashRouter使用hash表示路径，比较方便
+ *   这个区别和vue的router区别是一致的。
+ *  通常将根组件包裹在Router中使用router
+ * 
+ * 
+ * Router Matchers
+ *  <Switch> 和 <Route>
+ *   主要区别：
+ *      Switch在自己的字Route中查找匹配的第一个Route去渲染，其他的Route都不再渲染
+ *      Route是你匹配到了就会去渲染
+ *   注意：
+ *   <Route path="/"> 总会渲染URL,我们建议把它房子<Switch></Switch>的最后一项，或者
+ *      <Route exact path="/">中
+ *   支持Router在Switch外面，但是不建议这么使用
+ * 
+ * Navigator
+ *  Link，NavLink，Redirect
+ *   区别：
+ *    NavLink当前link被匹配中之后可以增加class
+ *    Redirect会覆盖当前的history stack,而不是像link产生一个新的跳转
  * 
  */
