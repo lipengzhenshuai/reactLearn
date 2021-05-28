12afef58427d7d124f7e2f42b2cfd094785a4147
李鹏 <lipeng@lipengdeMacBook-Pro.local>
2021-05-28 20:00:57 +0800

    add:增加typescript支持

--- src/pages/hook/1.tsx
+++ src/pages/hook/1.tsx
@@ -9,12 +9,14 @@ const Demo: FC<DemoProps> = (props) => {
     const inputElement = useRef();
 
     const handleFocusInput = () => {
+        //  @ts-ignore
         inputElement?.current.focus();
+        //  @ts-ignore
         console.log(inputElement?.current.value);
     }
 
     return (<div>
-        <input ref={inputElement} ></input><br/>
+        {/* <input ref={inputElement} ></input><br/> */}
         <button onClick={handleFocusInput}>1111</button>
     </div>);
 };
