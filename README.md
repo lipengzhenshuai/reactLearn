d4a77fedfad5e424ac4c6a1710a1aa85174bf737
李鹏 <lipeng0@aixuexi.com>
2022-07-20 14:42:19 +0800

    feat: add code

---
+++ README.md
@@ -0,0 +1,3 @@
+# React 知识从基础到高级Demo库
+
+## 开始 src/pages/core 基础知识
--- src/pages/redux/saga/component/Counter.jsx
+++ src/pages/redux/saga/component/Counter.jsx
@@ -1,11 +1,15 @@
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from 'react-redux';
 const Counter = (props) => {
+
+  const count = useSelector(state => state.count)
+  const dispatch = useDispatch()
+
   return (
     <div>
-      <p>{props.count}</p>
-      <button onClick={() => props.dispatch({ type: "ADD" })}>+</button>
-      <button onClick={() => props.dispatch({ type: "ASYNC_ADD" })}>+</button>
+      <p>{count}</p>
+      <button onClick={() => dispatch({ type: "ADD" })}>+</button>
+      <button onClick={() => dispatch({ type: "ASYNC_ADD" })}>+</button>
     </div>
   );
 };
-export default connect((state) => state)(Counter);
+export default Counter;
